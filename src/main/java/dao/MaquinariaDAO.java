2c0e4599e52431c9a788be4f5a59cfee4a852fac
NereidaRO <nereidarodriguezorenes@gmail.com>
2026-02-10 17:23:30 +0100

    added Machinery DAO interfaces

---
+++ src/main/java/dao/MaquinariaDAO.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package dao;
+import java.util.List;
+import java.util.Optional;
+import modelo.Maquinaria;
+
+/**
+ * Las funciones básicas son: crear, modificar, eliminar y listar. Esta lista, además, debe poderse filtrar, así que
+ * vamos a añadir un método de búsqueda por ID (buscar una máquina concreta), por tipo de máquina y por su estado. Como queremos que el
+ * filtro pueda incluir varios campos (por ejemplo, máquinas cortadoras averiadas), el método de filtrado será único
+ * 
+ * @author Nereida Rodríguez Orenes 2ºDAM
+ */
+public interface MaquinariaDAO {
+    void insertar(Maquinaria m);
+    void modificar(Maquinaria m);
+    void eliminar(Maquinaria m);
+    List<Maquinaria> listarMaquinaria();
+    Optional<Maquinaria> filtrarMaquinaria(int maquinariaID, int tipoMaquinariaID, int estadoMaquinariaID);
+}
---
+++ src/main/java/dao/TipoMaquinariaDAO.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package dao;
+import java.util.List;
+import modelo.TipoMaquinaria;
+
+/**
+ *Las tablas maestras no deben modificarse, o al menos no a menudo, pero, como pretendemos que esta aplicación sea escalable,
+ * debemos pensar en qué pasaría si en un tiempo el taller cliente necesita añadir algún tipo más al catálogo.
+ * Por ello, vamos a crear las funciones de añadir, modificar y eliminar, aunque controlaremos mediante la interfaz el acceso a estas
+ * para que sea limitado.
+ * 
+ * @author Nereida rodríguez Orenes 2ºDAM
+ */
+public interface TipoMaquinariaDAO {
+    void insertar(TipoMaquinaria t);
+    void eliminar (TipoMaquinaria t);
+    void modificar(TipoMaquinaria t);
+    List<TipoMaquinaria> listarTipoMaquinaria();
+    
+}
